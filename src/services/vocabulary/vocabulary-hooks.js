66d7144458684d218e031cf5d667a3d4e98301aa
forafish <fora.luo@gmail.com>
2018-03-17 23:44:38 +0800

    add cache to vocabulary

--- src/services/vocabulary/vocabulary-hooks.js
+++ src/services/vocabulary/vocabulary-hooks.js
@@ -1,8 +1,12 @@
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
 
 module.exports = function(options = {}) {
   return {
     before: {
+      all: [
+        cache(options.cache)
+      ],
       create: [
         hooks.authenticate('jwt', options.auth)
       ],
@@ -18,6 +22,7 @@ module.exports = function(options = {}) {
     },
     after: {
       all: [
+        cache(options.cache),
         hooks.responder()
       ]
     }
