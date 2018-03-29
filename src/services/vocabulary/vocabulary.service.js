0e230d82a4ce9b960a4a173b7af101ca9d0a61f1
forafish <fora.luo@gmail.com>
2018-03-29 16:39:30 +0800

    Fix this.options

--- src/services/vocabulary/vocabulary.service.js
+++ src/services/vocabulary/vocabulary.service.js
@@ -15,8 +15,7 @@ const defaultOptions = {
 class VocabularyService {
   constructor (options) {
     this.options = Object.assign({}, defaultOptions, options);
-    this.name = options.name;
-    this.options = options;
+    this.name = this.options.name;
     this.vocabularies = this.options.vocabularies || {};
   }
 
