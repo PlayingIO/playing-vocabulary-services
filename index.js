a045d3fab262a11bc59d89fa4177e817d193f8be
forafish <fora.luo@gmail.com>
2018-04-02 03:10:07 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require = require("esm")(module/*, options*/);
+console.time('playing-vocabulary-services import');
 module.exports = require('./src/index').default;
 module.exports.entities = require('./src/entities').default;
 module.exports.models = require('./src/models').default;
+console.timeEnd('playing-vocabulary-services import');
