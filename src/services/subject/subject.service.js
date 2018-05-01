c0d10b533cfcf81b22fdb54fd03393089726333b
forafish <fora.luo@gmail.com>
2018-05-01 23:32:38 +0800

    Fix deep clone options

--- src/services/subject/subject.service.js
+++ src/services/subject/subject.service.js
@@ -10,7 +10,7 @@ const defaultOptions = {
 
 export class SubjectService extends Service {
   constructor (options) {
-    options = Object.assign({}, defaultOptions, options);
+    options = fp.assign(defaultOptions, options);
     super(options);
   }
 
@@ -21,7 +21,7 @@ export class SubjectService extends Service {
 }
 
 export default function init (app, options, hooks) {
-  options = Object.assign({ ModelName: 'subject' }, options);
+  options = fp.assign({ ModelName: 'subject' }, options);
   return createService(app, SubjectService, SubjectModel, options);
 }
 
