ef576ce4919972a66ea3f5df220e267772b625e6
forafish <fora.luo@gmail.com>
2018-06-26 16:20:54 +0800

    async methods of vocabulary service

--- src/services/vocabulary/vocabulary.service.js
+++ src/services/vocabulary/vocabulary.service.js
@@ -24,11 +24,11 @@ export class VocabularyService {
     this.hooks(defaultHooks(this.options));
   }
 
-  find (params) {
+  async find (params) {
     return Promise.resolve(this.vocabularies);
   }
 
-  get (id, params) {
+  async get (id, params) {
     params = { query: {}, ...params };
     let name = id;
     const voc = params.action;
@@ -50,17 +50,16 @@ export class VocabularyService {
         params.query = fp.omit(['name', 'dbl10n', 'localize', 'lang', 'term'], params.query);
       }
       params.query.$sort = params.query.$sort || { parent: 1, position: 1 };
-      return this.app.service(service).find(params).then(results => {
-        return fp.map(entry => {
-          entry.dbl10n = params.query.dbl10n === true;
-          entry.displayLabel = entry[label];  // i10n
-          return entry;
-        }, fp.propOf('data', results) || []);
-      });
+      const resutls = await this.app.service(service).find(params);
+      return fp.map(entry => {
+        entry.dbl10n = params.query.dbl10n === true;
+        entry.displayLabel = entry[label];  // i10n
+        return entry;
+      }, fp.propOf('data', results) || []);
     }
   }
 
-  create (data, params) {
+  async create (data, params) {
     assert(data.type, 'data.type not provided');
     assert(fp.find(fp.propEq('name', data.type), this.vocabularies), `vocabulary ${data.type} not exists`);
 
@@ -69,7 +68,7 @@ export class VocabularyService {
     return this.app.service(service).create(data, params);
   }
 
-  update (id, data, params) {
+  async update (id, data, params) {
     assert(data.type, 'data.type not provided');
     assert(fp.find(fp.propEq('name', data.type), this.vocabularies), `vocabulary ${data.type} not exists`);
 
@@ -78,7 +77,7 @@ export class VocabularyService {
     return this.app.service(service).update(id, data, params);
   }
 
-  patch (id, data, params) {
+  async patch (id, data, params) {
     assert(data.type, 'data.type not provided');
     assert(fp.find(fp.propEq('name', data.type), this.vocabularies), `vocabulary ${data.type} not exists`);
 
@@ -87,7 +86,7 @@ export class VocabularyService {
     return this.app.service(service).patch(id, data, params);
   }
 
-  remove (id, params) {
+  async remove (id, params) {
     params = { query: {}, ...params };
     const type = id;
     const voc = params.action;
